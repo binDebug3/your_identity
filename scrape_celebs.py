01a46bcc14365c6536eb66eff2b70badd045e8b7
Dallin Stewart <dallinpstewart@gmail.com>
2023-03-24 20:27:27 -0600

    save celebs

--- scrape_celebs.py
+++ scrape_celebs.py
@@ -1,3 +1,5 @@
+import os
+
 import urllib.request
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -5,14 +7,51 @@ from selenium.webdriver.common.by import By
 
 class Celebrities(object):
     def __init__(self):
-        self.website = "https://www.imdb.com/list/ls058011111/?sort=list_order,asc&mode=grid&page=1&ref_=nmls_vw_grd"
+        self.website = "https://www.imdb.com/list/ls058011111/?sort=list_order,asc&mode=grid&page=1"
         self.driver = webdriver.Chrome(
             executable_path=r"C:\Users\dalli\PycharmProjects\CarMarket\Casper\chromedriver_win32\chromedriver.exe")
 
         self.driver.get(self.website)
 
-        self.names = self.findNames()
-        self.links = self.findLinks()
+        self.names = []
+        self.links = []
+        self.pageCount = 1
+
+
+    def scrape(self):
+        """
+        Scrape the website for celebrity names and links to their images
+        :return:
+        """
+        for i in range(10):
+            self.names = self.names + self.findNames()
+            self.links = self.links + self.findLinks()
+            print("Iteration: " + str(i))
+            print(len(self.names))
+            print(len(self.links), "\n")
+            self.driver.get(self.getNextPage())
+        self.driver.close()
+
+
+    def getNextPage(self):
+        """
+        Click the next button on the page
+        :return:
+        """
+        nextPage = self.website
+        index = -1
+        while nextPage[index].isdigit():
+            index -= 1
+        index += 1
+
+        # find the next page number
+        currStart = nextPage[index:]
+        newStart = str(int(currStart) + self.pageCount)
+
+        # update website link
+        site = nextPage[:len(nextPage) + index] + newStart
+        self.website = site
+        return site
 
 
     def findNames(self):
@@ -54,14 +93,34 @@ class Celebrities(object):
         Save the images of each celebrity
         :return: None
         """
-        for name, link in self.names, self.links:
-            path = "celebrities/" + "_".join(name.split()) + ".png"
-            urllib.request.urlretrieve(link, path)
+        for i, name in enumerate(self.names):
+            alt = "_".join(name.split())
+            path = "celebrities/" + alt
+            # dir_path = os.path.dirname(path)
+
+            # Check if the directory exists, if not create it and save the image
+            if not os.path.exists(path):
+                os.makedirs(path)
+                path += "/" + alt + ".jpg"
+                urllib.request.urlretrieve(self.links[i], path)
+
+    def save_names(self):
+        """
+        Save the names of each celebrity
+        :return: None
+        """
+        with open("celebrities/names.txt", "w") as file:
+            for name in self.names:
+                file.write("_".join(name.split()) + "\n")
 
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     celebs = Celebrities()
-    print(celebs.names)
-    print(celebs.links)
+    celebs.scrape()
+    celebs.save_names()
 
-    celebs.driver.close()
+
+    # celebs.save_images()
+    # print(celebs.names)
+    # print(celebs.links)
+
