dee3b3d3ef95ce16b8311d1c2238a9f7050facf0
jeffxhansen <jeffxhansen@gmail.com>
2023-03-24 18:41:19 -0600

    syncing

---
+++ gits.sh
@@ -0,0 +1,23 @@
+# if no command line arguments
+if [ $# -eq 0 ]
+  then
+    # add commit pull push with a default commit message
+    echo "No message supplied"
+    git add .
+    git commit -m "pushing"
+    git pull origin main
+    git push origin main
+fi
+
+# if a commit message was passed in as a command-line argument
+if [ $# -eq 1 ]
+  then
+    # add commit pull push with the passed-in commit
+    # message from the command line
+    git add .
+    git commit -m "$1"
+    git pull origin main
+    git push origin main
+fi
+
+
